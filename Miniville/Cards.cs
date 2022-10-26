4beaffc1edfadbbcfcd4ede42a7d330938a85ef0
Diane <102394629+makazemafuu@users.noreply.github.com>
2022-10-26 15:12:19 +0200

    Initialisation de la class Cards (+ enum Color et méthode ActiveEffect)

---
+++ Miniville/Cards.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miniville
+{
+    public class Cards
+    {
+        enum Color
+        {
+            Blue,
+            Green,
+            Red,
+            Purple
+        }
+
+        int activationValue;
+        string name;
+        int type;
+        int cost;
+        string effectDescription;
+
+        public void ActiveEffect()
+        {
+
+        }
+
+    }
+}
