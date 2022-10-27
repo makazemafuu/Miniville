c09f9a88678680907357fa2ad841cce2c9c7a9ba
Oceane <oomgames.d3@gmail.com>
2022-10-27 09:40:57 +0200

    squelette de player

---
+++ Miniville/Player.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miniville
+{
+    internal class Player : Bank
+    {
+        private string namePlayer;
+        private bool isAI;
+
+        private void Shop(Cards cartToBuy)
+        {
+            // on lui donne la carte à acheter en argument
+
+            // on retire au joueur le coût de la carte
+            //Player.coinsAvailable -= cartToBuy.cost;
+
+            // on rend l'argent à la banque
+            //Bank.coinsAvailable += cartToBuy.cost;
+
+            // on ajoute la carte à la main
+
+
+
+        }
+
+        private void DisplayChoice()
+        {
+            //
+        }
+
+        private void PlayerChoice()
+        {
+            //
+        }
+    }
+}
