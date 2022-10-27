b379394e653bbfd888cff271d2a08c5e7902c112
Guillaume Patrice <99061060+Stormahaaz1515@users.noreply.github.com>
2022-10-27 11:32:32 +0200

    Adding the base of player

--- Miniville/Player.cs
+++ Miniville/Player.cs
@@ -6,7 +6,35 @@ using System.Threading.Tasks;
 
 namespace Miniville
 {
-    internal class Class1
+    internal class Player : Bank
     {
+        private string namePlayer;
+        private bool isAI;
+
+        private void Shop(Card cartToBuy)
+        {
+            // on lui donne la carte à acheter en argument
+
+            // on retire au joueur le coût de la carte
+            //Player.coinsAvailable -= cartToBuy.cost;
+
+            // on rend l'argent à la banque
+            //Bank.coinsAvailable += cartToBuy.cost;
+
+            // on ajoute la carte à la main
+
+
+
+        }
+
+        private void DisplayChoice()
+        {
+            //
+        }
+
+        private void PlayerChoice()
+        {
+            //
+        }
     }
 }
