780ee058680b690d91ceb1882dd1706bbd986d37
Guillaume Patrice <99061060+Stormahaaz1515@users.noreply.github.com>
2022-10-27 11:25:25 +0200

    Adding main

--- Miniville/Program.cs
+++ Miniville/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Miniville
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Bank bank = new Bank();
+            bank.DisplayRessources();
+            bank.CardsAvailable["Verger"].PileCards.Pop();
+            bank.DisplayRessources();
+        }
+    }
+}
