f231e33a62b7411621c7a7696d1698633377267f
Guillaume Patrice <99061060+Stormahaaz1515@users.noreply.github.com>
2022-10-27 09:36:46 +0200

    Adding Bank and Pile

---
+++ Miniville/Bank.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Miniville
+{
+	internal class Bank 
+	{
+		private int coinsAvailable = 250;
+		private Dictionary<Pile, string> cardsAvailable = Pile.InitPile();
+
+		public void DisplayCards()
+		{
+			foreach (var card in cardsAvailable)
+				Console.WriteLine(card.Value);
+		}
+		public Dictionary<Pile, string> CardsAvailable
+		{
+			get { return this.cardsAvailable; }
+			set { this.cardsAvailable = value; }
+		}
+		public int CoinsAvailable
+		{
+			get { return this.coinsAvailable; }
+			set { this.coinsAvailable = value; }
+		}
+
+    }
+}
