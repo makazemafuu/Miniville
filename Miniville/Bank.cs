da3bb827c1f1813a877e7bf0ced96966fb940268
Guillaume Patrice <99061060+Stormahaaz1515@users.noreply.github.com>
2022-10-28 14:31:29 +0200

    Update Bank.cs

--- Miniville/Bank.cs
+++ Miniville/Bank.cs
@@ -9,7 +9,7 @@ namespace Miniville
 {
 	internal class Bank 
 	{
-		protected int coinsAvailable = 250;
+		protected int coinsAvailable;
 		protected Game game;
         protected Dictionary<string, Pile> cardsAvailable;
 		private Pile pile = new Pile();
@@ -46,7 +46,7 @@ namespace Miniville
 				Console.WriteLine("There is {0} cards of the type {1} remaining of the type in the Bank", card.Value.PileCards.Count, card.Key);
 		}
 
-		public void Trade(Bank from, Bank to,)
+		public void Trade(Bank from, Bank to, string type, string value)
 		{
 
 		}
